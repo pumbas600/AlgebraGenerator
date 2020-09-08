0e9d8facd40a9b3c5cd894bf41174f618680ed7c
pumbas600 <38820160+pumbas600@users.noreply.github.com>
2020-09-08 22:14:11 +1200

    Identified area where it can get stuck in infinite loop.

--- AlgebraGenerator.py
+++ AlgebraGenerator.py
@@ -1,8 +1,9 @@
+import math
 import random
-import math, time
+import time
 
-brackets_left = 3
-brackets_right = 2
+brackets_left = 2
+brackets_right = 1
 
 is_integer_answer = True
 maxX = 12
@@ -22,6 +23,8 @@ def generate_random_number(func, max_value):
         rn = func(0, max_value)
         if rn != 0:
             return random_sign(rn)
+        else:
+            pass
 
 def generate_x(integer_answer):
     if integer_answer:
@@ -31,14 +34,16 @@ def generate_x(integer_answer):
     return x
 
 
-def get_display_number(n, with_sign=True):
+def get_display_number(n, with_sign=True, show_one=False):
     if n == 0:
         return 0
 
     if n == 1 or n == -1:
         if with_sign:
-            return '+' if n > 0 else '-'
-        return '' if n > 0 else '-'
+            sign = '+' if n > 0 else '-'
+            return sign + '1' if show_one else sign
+        sign = '' if n > 0 else '-'
+        return sign + '1' if show_one else sign
     else:
         if with_sign:
             return f'+{n}' if n > 0 else str(n)
@@ -57,14 +62,24 @@ def generate_rhs(brackets_right, x):
         rhs_total += c * (a * x + b)
         # Only add a space before the brackets if this is not the first set of brackets.
         rhs += (str(c) if n == 0 else f' {get_display_number(c)}') \
-            + f'({get_display_number(a, False)}x {get_display_number(b)})'
+            + f'({get_display_number(a, False)}x {get_display_number(b, show_one=True)})'
 
     return rhs, rhs_total
 
 
+def generate_random_distributions(n):
+
+    # Distribution function is 2(x-0.5)^3 + 0.5, which gives values between 0.25 and 0.75 for values of x between
+    # 0 and 1. This distribution causes most values to be grouped around 0.5, but most of the values are very similar.
+    distribution_function = lambda x: 2 * math.pow(x - 0.5, 3) + 0.5
+    #distribution_function = lambda x: x
+    random_distribution = [distribution_function(generate_random_number(random.uniform, 1)) for _ in range(n)]
+    return random_distribution
+
+
 def distribute_total(brackets, total):
     while True:
-        random_distribution = [generate_random_number(random.uniform, 1) for _ in range(brackets)]
+        random_distribution = generate_random_distributions(brackets)
         # If by chance, the sum is 0, this will cause a divide by zero error, so we need to
         # regenerate the random numbers until their sum isn't 0.
 
@@ -81,6 +96,10 @@ def distribute_total(brackets, total):
             rounded = random_sign(1)
         distribution.append(rounded)
 
+    print('distributed')
+
+    #TODO: Find infinite loop in below code snippet (up until the return)
+
     # Check for rounding errors which may cause the total of the results
     # to not equal rhs_total
     sum_results = sum(distribution)
@@ -92,6 +111,7 @@ def distribute_total(brackets, total):
                 break
         distribution[index] += total - sum_results
         # print('Sum didnt equal total')
+    print('sum == total')
     return distribution
 
 
@@ -101,6 +121,9 @@ def find_factors(n):
         if n % x == 0:
             factors.append(int(x))
             factors.append(int(n / x))
+
+        if x > 100:
+            pass
     return factors
 
 
@@ -127,17 +150,24 @@ def generate_side(distributions, x):
 
 
 if __name__ == '__main__':
-    if x == 0: x = generate_x(is_integer_answer)
-
     random.seed = time.time()
-    rhs_total = generate_random_number(random.randint, max_rhs_total)
-    rhs_distributions = distribute_total(brackets_right, rhs_total)
-    rhs = generate_side(rhs_distributions, x)
+    for _ in range(25):
+        x = generate_x(is_integer_answer)
 
-    lhs_distributions = distribute_total(brackets_left, rhs_total)
-    # print(lhs_distributions)
-    lhs = generate_side(lhs_distributions, x)
+        rhs_total = generate_random_number(random.randint, max_rhs_total)
+        print('total')
+        rhs_distributions = distribute_total(brackets_right, rhs_total)
+        print('rhs d')
+        rhs = generate_side(rhs_distributions, x)
+        print('rhs')
 
-    print(f'{lhs} = {rhs}')
-    print(f'Answer is x = {x}')
-    print(f'Seed is: {random.seed}')
+        lhs_distributions = distribute_total(brackets_left, rhs_total)
+        print(lhs_distributions, rhs_distributions)
+        lhs = generate_side(lhs_distributions, x)
+
+        print(f'{lhs} = {rhs}')
+        # print(f'Answer is x = {x}')
+        # print(f'Seed is: {random.seed}')
+    print('Complete')
+
+    #TODO: Fractions - https://cboard.cprogramming.com/c-programming/158102-program-convert-decimal-fraction.html
